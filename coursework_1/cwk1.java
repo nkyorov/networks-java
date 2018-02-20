cd9d36950cc32ebca393779f5393c2fa0dc93ed4
Nikolay Kyorov <sc16nsk@leeds.ac.uk>
2018-02-20 15:49:31 +0000

    Common hierarchy

--- coursework_1/cwk1.java
+++ coursework_1/cwk1.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.net.*;
 import java.io.*;
 
-
 public class cwk1 {
     //Initial declaratioms
     private Scanner input = null;
@@ -10,6 +9,9 @@ public class cwk1 {
     private String[] segmented = null;
     private Scanner kbdReader = null;
     private InetAddress inet = null;
+    private byte[] address = null;
+    private List<String> list = new ArrayList<String>();
+
 
     /**
      * Default constructor for Scanner
@@ -28,6 +30,9 @@ public class cwk1 {
             return cmd;
         }
     }
+    public List getList(){
+        return list;
+    }
 
     /**
      * Handles multiple addresses, with space delimeter.
@@ -45,7 +50,7 @@ public class cwk1 {
      * @return Version of address
      */
     public String checkVersion(){
-        byte[] address = inet.getAddress();
+        address = inet.getAddress();
         if (address.length == 4) {
             return "IPv4";
         }
@@ -53,6 +58,7 @@ public class cwk1 {
             return "IPv6";
         }
     }
+
 
     /**
      * Provide required output and call relevant InetAddress functions
@@ -63,12 +69,19 @@ public class cwk1 {
             inet = InetAddress.getByName( host );
             byte[] address = inet.getAddress();
 
+            String ipa = inet.getHostAddress();
+            String[] res = ipa.split("\\.");
+            for(String w : res) {
+                list.add(w);
+            }
+
             System.out.println("========================");
             System.out.println( "Host name : " + inet.getHostName   () );
             System.out.println( "IP Address: " + inet.getHostAddress() );
             System.out.println( "IP version: " + checkVersion());
             System.out.println( "Reachable: " + inet.isReachable(60) );
             System.out.println("========================\n");
+
         }
 
         //Handles any exceptions related to InetAddress
@@ -82,10 +95,61 @@ public class cwk1 {
         }
     }
 
+    public String hier(){
+
+        boolean starAtOne = false;
+        boolean starAtTwo = false;
+        boolean starAtThree = false;
+        boolean starAtFour = false;
+
+        for (int i = 1; ; i++ ) {
+            while (i < list.size()) {
+                int index = (3 * i) + 1;
+                int index2 = (3 * i) - 1;
+
+                if (list.get(0) == list.get(index)) {
+                    starAtOne = true;
+                } else {
+                    starAtOne = false;
+                }
+                if (list.get(1) == list.get(index2)) {
+                    starAtTwo = true;
+                } else {
+                    starAtTwo = false;
+                }
+                if (list.get(2) == list.get(index2)) {
+                    starAtThree = true;
+                } else {
+                    starAtThree = false;
+                }
+                if (list.get(3) == list.get(index2)) {
+                    starAtFour = true;
+                } else {
+                    starAtFour = false;
+                }
+
+                if((starAtOne && starAtTwo && starAtThree && starAtFour) == true){
+                    return "*.*.*.*";
+                }
+                if(((starAtOne && starAtTwo && starAtThree)== true) && (starAtFour == false)){
+                    return "*.*.*.";
+                }
+                if(((starAtOne && starAtTwo)== true) && ((starAtFour && starAtThree) == false)){
+                    return "*.*..";
+                }
+                if((starAtOne == true) && ((starAtFour && starAtThree && starAtTwo) == false)){
+                    return "*...";
+                }
+                else {
+                    return "No common hierarchy.";
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         cwk1 kbd = new cwk1();
         cwk1 lookup = new cwk1();
-
         // Reads one or more hostnames provided as command line argument
         if (args.length != 0) {
             for (int i = 0; i<args.length ;i++) {
@@ -98,6 +162,9 @@ public class cwk1 {
             for (String segment: seg) {
                 lookup.resolve(segment);
             }
+        }
+        if (args.length > 1) {
+
         }
     }
 }
